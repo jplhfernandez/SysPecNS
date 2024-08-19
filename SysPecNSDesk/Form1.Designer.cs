f4f473cc3d259c01b664d28723700472d49635ba
joao.plhfernandez <joao.plhfernandez@ITQ0675987W10-1>
2024-08-19 09:08:32 -0300

    Adicionar arquivos de projeto.

---
+++ SysPecNSDesk/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace SysPecNSDesk
+{
+    partial class Form1
+    {        /// <summary>
+
+        ///  Required designer variable.        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            bntConfirma = new Button();
+            txtNumero2 = new TextBox();
+            lstResultado = new ListBox();
+            cmbOperacao = new ComboBox();
+            txtNumero1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // bntConfirma
+            // 
+            bntConfirma.Enabled = false;
+            bntConfirma.Location = new Point(478, 326);
+            bntConfirma.Name = "bntConfirma";
+            bntConfirma.Size = new Size(75, 23);
+            bntConfirma.TabIndex = 0;
+            bntConfirma.Text = "&Confirma";
+            bntConfirma.UseVisualStyleBackColor = true;
+            bntConfirma.Click += button1_Click;
+            // 
+            // txtNumero2
+            // 
+            txtNumero2.Location = new Point(453, 92);
+            txtNumero2.Name = "txtNumero2";
+            txtNumero2.Size = new Size(100, 23);
+            txtNumero2.TabIndex = 1;
+            txtNumero2.TextChanged += textBox1_TextChanged;
+            // 
+            // lstResultado
+            // 
+            lstResultado.FormattingEnabled = true;
+            lstResultado.ItemHeight = 15;
+            lstResultado.Location = new Point(211, 121);
+            lstResultado.Name = "lstResultado";
+            lstResultado.Size = new Size(342, 199);
+            lstResultado.TabIndex = 2;
+            lstResultado.SelectedIndexChanged += lstResultado_SelectedIndexChanged;
+            // 
+            // cmbOperacao
+            // 
+            cmbOperacao.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbOperacao.FormattingEnabled = true;
+            cmbOperacao.Items.AddRange(new object[] { "Multiplicação", "Divisão", "Adição", "Subtração", "Exponenciação", "Radiciação", "Logaritmos" });
+            cmbOperacao.Location = new Point(211, 92);
+            cmbOperacao.Name = "cmbOperacao";
+            cmbOperacao.Size = new Size(121, 23);
+            cmbOperacao.TabIndex = 3;
+            cmbOperacao.SelectedIndexChanged += cmbOperacao_SelectedIndexChanged;
+            // 
+            // txtNumero1
+            // 
+            txtNumero1.Location = new Point(343, 92);
+            txtNumero1.Name = "txtNumero1";
+            txtNumero1.Size = new Size(100, 23);
+            txtNumero1.TabIndex = 4;
+            txtNumero1.TextChanged += txtNumero1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(343, 74);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 15);
+            label1.TabIndex = 5;
+            label1.Text = "1°Número";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(453, 74);
+            label2.Name = "label2";
+            label2.Size = new Size(62, 15);
+            label2.TabIndex = 6;
+            label2.Text = "2°Número";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(211, 74);
+            label3.Name = "label3";
+            label3.Size = new Size(58, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Operação";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 453);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtNumero1);
+            Controls.Add(cmbOperacao);
+            Controls.Add(lstResultado);
+            Controls.Add(txtNumero2);
+            Controls.Add(bntConfirma);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button bntConfirma;
+        private TextBox txtNumero2;
+        private ListBox lstResultado;
+        private ComboBox cmbOperacao;
+        private TextBox txtNumero1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+
+}
---
+++ SysPecNSDesk/Form1.cs
@@ -0,0 +1,122 @@
+namespace SysPecNSDesk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //   MessageBox.Show("Olá TI96");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (txtNumero2.Text.Length > 0)
+            {
+                bntConfirma.Enabled = true;
+            }
+            else
+            {
+                bntConfirma.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string operacao = cmbOperacao.Text;
+            int posicaoOperacao = cmbOperacao.SelectedIndex;
+            double numero2 = 0;
+            double resultado = 0;
+            double numero1 = 0;
+
+            if (txtNumero1.Text != string.Empty)
+            {
+                numero1 = Convert.ToDouble(txtNumero1.Text);
+            }
+
+            if (posicaoOperacao < 5)
+            {
+                if (txtNumero2.Text != string.Empty)
+                {
+                    numero2 = Convert.ToDouble(txtNumero2.Text);
+                }
+            }
+
+            switch (posicaoOperacao)
+            {
+                case 0:
+                    resultado = numero1 * numero2;
+                    break;
+                case 1:
+                    resultado = numero1 / numero2;
+                    break;
+                case 2:
+                    resultado = numero1 + numero2;
+                    break;
+                case 3:
+                    resultado = numero1 - numero2;
+                    break;
+                case 4:
+                    resultado = Math.Pow(numero1, numero2);
+                    break;
+                case 5:
+                    resultado = Math.Sqrt(numero1);
+                    break;
+                case 6:
+                    resultado = Math.Log(numero1);
+                    break;
+            }
+
+
+            lstResultado.Items.Add($"{operacao} - {posicaoOperacao} = {resultado}");
+
+            //Existem 3 tipos de conversão no C#
+            // CAST
+            // PARSE
+            // CONVERT
+            //int numero = int.Parse(txtNumero1.Text);
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    lstResultado.Items.Add($"{numero} x {i + 1}= {numero * (i + 1)}");
+            //}
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNumero1_TextChanged(object sender, EventArgs e)
+        {
+            if (txtNumero1.Text.Length > 0)
+            {
+                bntConfirma.Enabled = true;
+            }
+        }
+
+        private void lstResultado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbOperacao_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbOperacao.SelectedIndex > 4)
+            {
+                txtNumero2.Enabled = false;
+            }
+            else
+            {
+                txtNumero2.Enabled = true;
+            }
+            txtNumero1.Clear();
+            txtNumero2.Clear();
+            txtNumero1.Focus();
+            bntConfirma.Enabled = false;
+        }
+    }
+}
+
