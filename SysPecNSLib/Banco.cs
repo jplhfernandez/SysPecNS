03660ab5baa30695fc8444b7c7418f9236a5c8d3
João Pedro <jplhfernandez@gmail.com>
2024-08-19 11:51:44 -0300

    Atualização no Banco e Usuario

--- SysPecNSLib/Banco.cs
+++ SysPecNSLib/Banco.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace SysPecNSLib
 {
-    public class Banco
+    public static class Banco// classe de conexão de banco de dados
     {
+        /// <summary>
+        /// Método abrir da classe Banco é utilizado para representar comandos SQL 
+        /// no servidor cujos os dados foram informados na string de conexão (strconn)
+        /// </summary>
+        /// <returns>Entrega um objeto de comandos SQL, que serão executados na conexão informada</returns>
+        public static MySqlCommand Abrir() // método de conexão
+        {
+            // daos de conexão
+            string strconn = @"server=127.0.0.1;database=syspecdb;user=root;password";
+            MySqlConnection cn = new MySqlConnection(strconn);
+            MySqlCommand cmd = new();
+            try// tratamento de excessão
+            {
+                cn.Open();
+                cmd.Connection = cn;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return cmd;
+        }
     }
 }
