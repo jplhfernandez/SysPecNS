6468765c223bb9e5c3e631d06600a152a7f76ed5
João Pedro <jplhfernandez@gmail.com>
2024-09-02 11:53:36 -0300

    criação da classe produto e categoria

--- SysPecNSDesk/FrmClienteEndereco.Designer.cs
+++ SysPecNSDesk/FrmClienteEndereco.Designer.cs
@@ -32,9 +32,27 @@
             groupBox1 = new GroupBox();
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            btnCancelar = new Button();
+            btnDeletar = new Button();
+            btnEditar = new Button();
+            dateTimePicker1 = new DateTimePicker();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            btnInserir = new Button();
+            checkBox1 = new CheckBox();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
             tabPage2 = new TabPage();
             groupBox1.SuspendLayout();
             tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
             SuspendLayout();
             // 
             // mySqlCommand1
@@ -47,12 +65,11 @@
             // groupBox1
             // 
             groupBox1.Controls.Add(tabControl1);
-            groupBox1.Location = new Point(183, 76);
+            groupBox1.Location = new Point(66, 12);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(428, 299);
+            groupBox1.Size = new Size(890, 466);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
-            groupBox1.Text = "groupBox1";
             // 
             // tabControl1
             // 
@@ -61,25 +78,186 @@
             tabControl1.Location = new Point(6, 22);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(416, 271);
+            tabControl1.Size = new Size(878, 427);
             tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
+            tabPage1.Controls.Add(btnCancelar);
+            tabPage1.Controls.Add(btnDeletar);
+            tabPage1.Controls.Add(btnEditar);
+            tabPage1.Controls.Add(dateTimePicker1);
+            tabPage1.Controls.Add(textBox5);
+            tabPage1.Controls.Add(textBox4);
+            tabPage1.Controls.Add(textBox3);
+            tabPage1.Controls.Add(textBox2);
+            tabPage1.Controls.Add(textBox1);
+            tabPage1.Controls.Add(btnInserir);
+            tabPage1.Controls.Add(checkBox1);
+            tabPage1.Controls.Add(label6);
+            tabPage1.Controls.Add(label5);
+            tabPage1.Controls.Add(label4);
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(label1);
             tabPage1.Location = new Point(4, 24);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(408, 243);
+            tabPage1.Size = new Size(870, 399);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Dados Clientes";
             tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Location = new Point(613, 271);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(75, 23);
+            btnCancelar.TabIndex = 17;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnDeletar
+            // 
+            btnDeletar.Location = new Point(613, 230);
+            btnDeletar.Name = "btnDeletar";
+            btnDeletar.Size = new Size(75, 23);
+            btnDeletar.TabIndex = 16;
+            btnDeletar.Text = "Deletar";
+            btnDeletar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            btnEditar.Location = new Point(500, 260);
+            btnEditar.Name = "btnEditar";
+            btnEditar.Size = new Size(75, 23);
+            btnEditar.TabIndex = 15;
+            btnEditar.Text = "Editar";
+            btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Location = new Point(165, 258);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(166, 189);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(186, 23);
+            textBox5.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(373, 123);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(133, 23);
+            textBox4.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(166, 123);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(186, 23);
+            textBox3.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(246, 70);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(260, 23);
+            textBox2.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(165, 70);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(58, 23);
+            textBox1.TabIndex = 9;
+            // 
+            // btnInserir
+            // 
+            btnInserir.Image = Properties.Resources.Add;
+            btnInserir.Location = new Point(399, 260);
+            btnInserir.Name = "btnInserir";
+            btnInserir.Size = new Size(75, 23);
+            btnInserir.TabIndex = 8;
+            btnInserir.Text = "Inserir";
+            btnInserir.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(452, 191);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(54, 19);
+            checkBox1.TabIndex = 7;
+            checkBox1.Text = "Ativo";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(159, 229);
+            label6.Name = "label6";
+            label6.Size = new Size(112, 15);
+            label6.TabIndex = 5;
+            label6.Text = "Data de nascimento";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(159, 160);
+            label5.Name = "label5";
+            label5.Size = new Size(36, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Email";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(373, 105);
+            label4.Name = "label4";
+            label4.Size = new Size(51, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Telefone";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(159, 99);
+            label3.Name = "label3";
+            label3.Size = new Size(28, 15);
+            label3.TabIndex = 2;
+            label3.Text = "CPF";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(246, 43);
+            label2.Name = "label2";
+            label2.Size = new Size(40, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Nome";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(159, 43);
+            label1.Name = "label1";
+            label1.Size = new Size(18, 15);
+            label1.TabIndex = 0;
+            label1.Text = "ID";
             // 
             // tabPage2
             // 
             tabPage2.Location = new Point(4, 24);
             tabPage2.Name = "tabPage2";
             tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(408, 243);
+            tabPage2.Size = new Size(870, 399);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "Enderecos";
             tabPage2.UseVisualStyleBackColor = true;
@@ -88,12 +266,14 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1020, 523);
             Controls.Add(groupBox1);
             Name = "FrmClienteEndereco";
             Text = "FrmClienteEndereco";
             groupBox1.ResumeLayout(false);
             tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -104,5 +284,22 @@
         private TabControl tabControl1;
         private TabPage tabPage1;
         private TabPage tabPage2;
+        private Button btnInserir;
+        private CheckBox checkBox1;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button btnCancelar;
+        private Button btnDeletar;
+        private Button btnEditar;
     }
 }
