3c48452f647cedeeb98675de6277072f35eaea33
João Pedro <jplhfernandez@gmail.com>
2024-09-13 08:40:07 -0300

    criação construtores estoque

--- SysPecNSLib/Estoque.cs
+++ SysPecNSLib/Estoque.cs
@@ -8,5 +8,25 @@ namespace SysPecNSLib
 {
     public class Estoque
     {
+        public int Produto_id { get; set; }
+        public double Quantidade { get; set; }
+        public DateTime? Data_ultimo_movimento { get; set; } = DateTime.Now;
+
+        public Estoque(int produto_id, double quantidade, DateTime? data_ultimo_movimento)
+        {
+            Produto_id = produto_id;
+            Quantidade = quantidade;
+            Data_ultimo_movimento = data_ultimo_movimento;
+        }
+
+        public Estoque()
+        {
+        }
+
+        public Estoque(double quantidade, DateTime? data_ultimo_movimento)
+        {
+            Quantidade = quantidade;
+            Data_ultimo_movimento = data_ultimo_movimento;
+        }
     }
 }
